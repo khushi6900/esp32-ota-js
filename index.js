2b3c366c79139ef6170f128f6a981bae3ae71229
Khushi Mahajan <khushimahajan.cs@gmail.com>
2026-02-16 15:27:53 +0530

    Add firmware version 3.1.0

--- index.js
+++ index.js
@@ -35,6 +35,12 @@ const AVAILABLE_FIRMWARE = {
     release_date: "2026-02-14",
     changelog: "Updated version",
     mandatory: true
+  },
+  "3.1.0": {
+    filename: "sketch_feb16_3_1.ino.esp32.bin",
+    release_date: "2026-02-16",
+    changelog: "New features and improvements",
+    mandatory: false
   }
 };
 
